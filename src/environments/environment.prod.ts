5e48ad698be67bad065131a38055ec043ac21694
Cristóbal <cristobal_jjg@hotmail.com>
2022-04-14 19:17:22 +0100

    Fallo técnico

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true,
+  firebase: {
+    projectId: 'pwm-angular-shoppin-web',
+    appId: '1:377193322204:web:42a0e4da7fc5b250290619',
+    databaseURL: 'https://pwm-angular-shoppin-web-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pwm-angular-shoppin-web.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBh-_vRNc3zn9Ric8mwmrQJ-iZxpAgQpL4',
+    authDomain: 'pwm-angular-shoppin-web.firebaseapp.com',
+    messagingSenderId: '377193322204',
+  },
+  production: false
 };
