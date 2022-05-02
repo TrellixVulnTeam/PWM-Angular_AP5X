c4b7aa9ed4ab0d00d2986ebb15cf3a00d9e5aa64
Cristóbal <cristobal_jjg@hotmail.com>
2022-05-02 19:13:57 +0100

    Cambios en la pantalla del objeto (NO esta terminado)

--- src/app/objeto/objeto.component.ts
+++ src/app/objeto/objeto.component.ts
@@ -1,6 +1,7 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FirestoreService } from 'src/services/firestore.service';
 import { JSONService } from 'src/services/json.service';
 @Component({
   selector: 'app-objeto',
@@ -9,33 +10,21 @@ import { JSONService } from 'src/services/json.service';
 })
 export class ObjetoComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private json:JSONService) { }
-  
-  private objeto:any;
-  
-  private productos:any;
-  private altIMG:any;
+  constructor(private route: ActivatedRoute, 
+    private json:JSONService, 
+    private dbService:FirestoreService){
+  }
+  public objeto:any;
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
-    
-    this.json.getJSONProducts()
-      .subscribe( resp => {
-        let aux:any = resp;
-        this.productos=aux.Productos;
-        console.log(id);
-        this.objeto = this.productos[id];
-        console.log(this.objeto);
 
-        document.getElementById("mainPhoto")?.setAttribute("src", this.objeto.mainPhoto);
-        document.getElementById('objName')!.textContent = this.objeto.nombre;
-        document.getElementById("precio")!.textContent = this.objeto.precio + "€";
-        document.getElementById("empresa")!.textContent = this.objeto.empresa;
-        const altPhotos = document.getElementById("altPhotos");
-        for(let i = 0; i< this.objeto.altPhotos; i++){
-          
-        }
-    });
+    const productos = this.dbService.getAllProducts();
+    productos.then((data) => {
+      this.objeto = data[id];
+      console.log(this.objeto)
+    })
   }
 
+  refresh():void { window.location.reload(); }
 }
